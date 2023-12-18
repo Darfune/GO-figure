e87d5e9bfcfa13bcfb2f2a76f134a0ce27c451f7
Darfune <harolep@gmail.com>
2023-12-18 22:42:52 +0200

    arrays, maps & slices

--- exercises/slices/slices.go
+++ exercises/slices/slices.go
@@ -16,7 +16,29 @@ package main
 
 import "fmt"
 
+func showLine(line []Part) {
+	for i := 0; i < len(line); i++ {
+		part := line[i]
+		fmt.Println(part)
+	}
+}
+
 type Part string
 
 func main() {
+	assemblyLine := make([]Part, 3)
+	assemblyLine[0] = "Pipe"
+	assemblyLine[1] = "Bolt"
+	assemblyLine[2] = "Sheet"
+
+	fmt.Println("3 parts:")
+	showLine(assemblyLine)
+
+	assemblyLine = append(assemblyLine, "Washer", "Wheel")
+	fmt.Println("\nAdded two parts:")
+	showLine(assemblyLine)
+	
+	assemblyLine = assemblyLine[3:]
+	fmt.Println("\nSliced parts:")
+	showLine(assemblyLine)
 }
