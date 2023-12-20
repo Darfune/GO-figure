7f80921ee019bbbc707e7a7c234c5c58f6c7e790
Darfune <harolep@gmail.com>
2023-12-20 22:08:37 +0200

    interfaces and errors

--- exercises/errors/errors.go
+++ exercises/errors/errors.go
@@ -15,3 +15,51 @@
 //* Use the `errors` package to generate errors
 
 package timeparse
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type Time struct {
+	hour, minute, second int
+}
+
+type TimeParseError struct {
+	msg, input string
+}
+
+func (t *TimeParseError) Error() string {
+	return fmt.Sprintf("%v: %v", t.msg, t.input)
+}
+
+func ParseTime(input string) (Time, error) {
+	components := strings.Split(input, ":")
+	if len(components) != 3 {
+		return Time{}, &TimeParseError{"Invalid number of time components", input}
+	} else {
+		hour, err := strconv.Atoi(components[0])
+		if err != nil {
+			return Time{}, &TimeParseError{fmt.Sprintf("Error parsing hour: %v", err), input}
+		}
+		minute, err := strconv.Atoi(components[0])
+		if err != nil {
+			return Time{}, &TimeParseError{fmt.Sprintf("Error parsing minutes: %v", err), input}
+		}
+		second, err := strconv.Atoi(components[0])
+		if err != nil {
+			return Time{}, &TimeParseError{fmt.Sprintf("Error parsing seconds: %v", err), input}
+		}
+		if hour > 23 || hour < 0 {
+			return Time{}, &TimeParseError{"hour out of range:", fmt.Sprintf("%v", hour)}
+		}
+		if minute > 59 || minute < 0 {
+			return Time{}, &TimeParseError{"minutes out of range:", fmt.Sprintf("%v", minute)}
+		}
+		if second > 59 || second < 0 {
+			return Time{}, &TimeParseError{"seconds out of range:", fmt.Sprintf("%v", second)}
+		}
+		return Time{hour, minute, second}, nil
+	}
+}
